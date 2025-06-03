39dcc623a686e35c17dceb3c179fd2217b6914be
LIN <1834067174@qq.com>
2025-06-03 23:48:11 +0800

    xiugaiyi

--- music/src/App.js
+++ music/src/App.js
@@ -4,13 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Music App</h1>
-        <p> Discover your favorite tunes and artists!</p>
-        <p>Explore the latest hits and classic tracks.</p>
-      </header>
+      <div className='zuo'>
+        <div className='zuo-top'>唱片区</div>
+        <div className='zuo-body'>
+          <div className="player">
+            <div className="player-controls1">
+              <button className="prev-btn">⏮️</button>
+              <button className="play-pause-btn">⏯️</button>
+              <button className="next-btn">⏭️</button>
+            </div>
+            <div className="player-controls2">
+              <button className="loop-btn">🔁</button>
+              <button className="lyrics-btn">词</button>
+              <button className="like-btn">❤️</button>
+            </div>
+          </div>
+        </div>
+        <div className="zuo-bottom">播放列表区</div>
+      </div>
+      <div className='you'>全屏歌词区</div>
     </div>
   );
+  
 }
 
 export default App;
